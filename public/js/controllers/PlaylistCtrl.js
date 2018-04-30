050d452cee50871996f47158261c0036eed574c8
rana-vemireddy <vemireddy.rana@gmail.com>
2018-04-30 00:20:19 -0400

    Got playlists to pull data from last.fm api

--- public/js/controllers/PlaylistCtrl.js
+++ public/js/controllers/PlaylistCtrl.js
@@ -1,6 +1,18 @@
-angular.module('instaDJ').controller('PlaylistCtrl', function($scope, $rootScope, $state) {
+angular.module('instaDJ').controller('PlaylistCtrl', function($scope, $rootScope, $state, $http) {
   $rootScope.$state = $state;
-  $scope.tracks = $rootScope.playlist.tracks;
+  $scope.tracks = []; //$rootScope.playlist.tracks;
+  $scope.tracks.push("1b60a734-79a7-4f7c-baa3-4ec637fa0729")
+  $scope.tracksInfo = [];
+  for(track in $scope.tracks){
+    var tempUrl = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=31019bd7e0a4251800e5ce24efb9d5ea&mbid="+$scope.tracks[track]+"&format=json";
+    $http({method: "GET", url: tempUrl}).then(function success(response){
+      console.log(response);
+      $scope.tracksInfo.push(response.data.track);
+    },
+    function error(response){
+      console.log(response);
+    });
+  };
     $scope.data = [
       {
         title: "a title",
